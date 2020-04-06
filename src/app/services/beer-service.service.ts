d94f09677a07622bec40f30d4a8a95f168eda8ae
cawhitecode <casoccerdude@gmail.com>
2020-04-06 10:40:17 -0500

    Changed Comments a bit

--- src/app/services/beer-service.service.ts
+++ src/app/services/beer-service.service.ts
@@ -112,14 +112,16 @@ export class BeerService {
     this.http.get<Beer[]>(this.APIPathRemove()).subscribe(v => this.beers.next(v));
   }
 
-  // Select what page and filter to apply to query then get from API string
-  // 0 = No filter and loads 
-  // 1 = 5% ABV or GREATER
-  // 2 = 5% ABV or LESS
-  // 3 = 50 IBU or GREATER
-  // 4 = 50 IBU or LESS
-  // Any other number not assigned will return all beers
-  // Page number is set up to save what page the client is on and return page based prev/next page
+  /* 
+    Select what page and filter to apply to query then get from API string
+    0 = No filter and loads 
+    1 = 5% ABV or GREATER
+    2 = 5% ABV or LESS
+    3 = 50 IBU or GREATER
+    4 = 50 IBU or LESS
+    Any other number not assigned will return all beers
+    Page number is set up to save what page the client is on and return page based prev/next page
+  */
   private APIPath(): string {
     let tempApiPath = `${this.path}?page=${this.page}&per_page=`
     if (this.filter == 0 || this.filter > 4){
@@ -144,19 +146,19 @@ export class BeerService {
   }
 
   // This checks for the filter based on the index of queryParams which is offset by one of filter
-  // queryParams = [ '&abv_gt=5', '&abv_lt=5', '&ibu_gt=50', '&ibu_lt=50'];
+  /// queryParams = [ '&abv_gt=5', '&abv_lt=5', '&ibu_gt=50', '&ibu_lt=50'];
   private CheckStringForDoubleQueryParams(filter: number) : boolean {
       return this.apiPath.includes(this.queryParams[filter - 1]);
     }
 
   // Removes filter based on filter input in correlation to queryParams
-  // queryParams = [ '&abv_gt=5', '&abv_lt=5', '&ibu_gt=50', '&ibu_lt=50'];
+  /// queryParams = [ '&abv_gt=5', '&abv_lt=5', '&ibu_gt=50', '&ibu_lt=50'];
   private RemoveFilterBasedOnFilterNumber(filter: number) {
     this.apiPath = this.apiPath.replace(this.queryParams[this.filter - 1], "");
   }
 
   // Checks for duplicate params to not query API twice and if no duplicates then add from queryParams to apiPath
-  // queryParams = [ '&abv_gt=5', '&abv_lt=5', '&ibu_gt=50', '&ibu_lt=50'];
+  /// queryParams = [ '&abv_gt=5', '&abv_lt=5', '&ibu_gt=50', '&ibu_lt=50'];
   private APIpathCheckingForDuplicateParamsAndConcat(filter: number, tempApiPath: string){
     if (this.CheckStringForDoubleQueryParams(filter))
     {
